e0c7e6547c60515013432e2cf2ba376850fafb22
Waleed33490 <sw79790@gmail.com>
2025-01-16 19:18:12 +0500

    Add files via upload

---
+++ lab 05/lab 5/Lab13/Lab13/Form1.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace Lab13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_input = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_input
+            // 
+            this.btn_input.Location = new System.Drawing.Point(177, 93);
+            this.btn_input.Name = "btn_input";
+            this.btn_input.Size = new System.Drawing.Size(75, 23);
+            this.btn_input.TabIndex = 0;
+            this.btn_input.Text = "input";
+            this.btn_input.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 275);
+            this.Controls.Add(this.btn_input);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_input;
+
+    }
+}
+
---
+++ lab 05/lab 5/Lab13/Lab13/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Lab13
+{
+    public partial class Form1 : Form
+    {
+
+
+        List<List<String>> Symboltable = new List<List<String>>();
+        ArrayList LineNumber;
+        ArrayList Variables;
+        ArrayList KeyWords;
+        ArrayList Constants;
+        ArrayList finalArray;
+        ArrayList tempArray;
+        Regex variable_Reg;
+        Regex constants_Reg;
+        Regex operators_Reg;
+        int lexemes_per_line;
+        int ST_index;
+
+        public Form1()
+        {
+            InitializeComponent();
+            String[] k_ = { "int", "float", "begin", "end", "print", "if", "else" };
+            ArrayList key = new ArrayList(k_);
+            LineNumber = new ArrayList();
+            Variables = new ArrayList();
+            KeyWords = new ArrayList();
+            Constants = new ArrayList();
+            finalArray = new ArrayList();
+            tempArray = new ArrayList();
+            variable_Reg = new Regex(@"^[A-Za-z|_][A-Za-z|0-9]*$");
+            constants_Reg = new Regex(@"^[0-9]+([.][0-9]+)?([e]([+|-])?[0-9]+)?$");
+            operators_Reg = new Regex(@"[+-/*=;>(){}]");
+            int L = 1;
+            Output.Text = "";
+            ST.Text = "";
+            Symboltable.Clear();
+            if_deleted = false;
+            string strinput = Input.Text;
+            char[] charinput = strinput.ToCharArray();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
